1154d031495b14600eb0adcea061977ac8651e9d
CyanWolf <2861205314@qq.com>
2024-08-16 15:58:49 +0800

    创建PIP版本前的修改

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name="clathon",
+    version="1.20.6",
+    description="""Clathon 是一个用 CPython 编写的 Python 解释器。它添加了很多功能(即编写
+Python 中的 Python，我被实现震撼了)
+设置和使用:
+使用前，别忘了将Clathon安装目录添加到PATH中!
+设置完成后，您可以使用 clathon 命令打开
+Clathon 交互式环境，或使用 clathon <file>运行文件
+Gitee 项目仓库的 URL:https://gitee.com/HardyProjects/clathon
+仓库地址:git@gitee.com:HardyProjects/clathon.git
+作者： Cyan Wolf
+日期:2024/8/16
+版本: 1.20.6
+描述:Clathon 是用 CPython 编写的 Python 解释器。""",
+    author="Hardy",
+    py_modules=["clathon","func"]
+)
